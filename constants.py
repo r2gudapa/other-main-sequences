847fa9a5a101cd143027292c257285ad12d45797
Rishita <r2gudapa@edu.uwaterloo.ca>
2017-03-19 23:17:30 -0400

    creation of constants file (python)

---
+++ constants.py
@@ -0,0 +1,63 @@
+# ALL CONSTANTS BELOW IN SI UNITS
+
+# Proton Mass
+m_p = 1.6726219e-27 
+
+# Electron Mass
+m_e = 9.109e-31 
+
+# Mass of the Sun
+M_s = 1.989e30 
+
+# Radius of the Sun
+R_s = 6.963e8 
+
+# Luminosity of the Sun
+L_s = 3.828e26 
+
+# Big G = gravitational constant
+G = 6.674e-11 
+
+# speed of light
+c = 2.998e8   
+
+# Boltzmann Constant
+k = 1.381e-23
+
+# Stefan-Boltzmann Constant
+stfb = 5.670e-8 
+
+# Radiation Energy Density Constant
+a = 4 * stfb / c
+
+# Just pi
+pi = np.pi
+
+# Adiabatic Index for Ideal Gas
+gamma = 5/3 
+
+# Planck's Constant
+h = 6.626e-34 
+
+# Reduced Planck's Constant
+hbar = h / (2 * np.pi)
+
+# HI -> HII ionization energy (IN eV)
+chi = 13.6
+
+############################################
+
+# VALUES OF MEAN MOLECULAR WEIGHT
+
+# fully ionized pure-H gas
+mu_HII = 1/2
+
+# fully ionized pure-He gas
+mu_HeIII = 4/3
+
+# Sun-like star
+mu_S = 0.612
+
+# Red Giant 
+mu_RG = 1.3 # (please check)
+ 
